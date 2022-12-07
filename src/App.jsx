f83c3726db16f1244ffdf776a2c97ebd876b2049
Adolfo Zamora Pescador <AdolfoZamora1005@gmail.com>
2022-12-07 02:25:37 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./components/NavBar"
+import "./index.css"
+
+
+function App() {
+
+  return (
+    <div>
+      {/* <NavBar/> */}
+      <h1 className="text-10xl font-bold underline flex">
+      yo soy el mejor programador
+    </h1>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'
+import logoinit from '../assets/img/logoinit.png'
+
+
+const NavBar = () => {
+    return (
+        // <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[black] text-gray-300'>
+        //     <div>
+        //         <img src={logoinit} alt="logo image" style={{ width: '50px' }} />
+        //     </div>
+        //     {/* Menu */}
+        //     <div>
+        //         <ul className='flex'>
+        //             <li>Home</li>
+        //             <li>About</li>
+        //             <li>Skills</li>
+        //             <li>Contact</li>
+        //         </ul>
+        //     </div>
+        //     {/* hamburger */}
+        //     <div className='hidden'>
+        //         <FaBars/>
+        //     </div>
+        //     {/* mobile menu */}
+        //     <ul className='hidden'>
+        //     <li>Home</li>
+        //             <li>About</li>
+        //             <li>Skills</li>
+        //             <li>Contact</li> 
+        //     </ul>
+        //     {/* social icons */}
+        //     <div></div>
+        // </div>
+
+
+
+    );
+};
+
+export default NavBar;
